6740673cc30cf740e16d78f5bd917c7df92998e8
Tyler J Hague <tjhague@gmail.com>
2018-11-05 20:55:32 -0500

    Pushing to test out Pass2 work. Passing density to replay script

--- batch_farm/get_density.cpp
+++ batch_farm/get_density.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
+#include "../replay/scripts/headers/SQLAnalysis.h"
+
+Double_t He3Boiling(Double_t I){
+  return (1 + (-0.004759*I) + (8.69e-05*I*I));
+}
+
+Double_t H3Boiling(Double_t I){
+  return (1 + (-0.007399*I) + (0.0001293*I*I));
+}
+
+Double_t D2Boiling(Double_t I){
+  return (1 + (-0.006651*I) + (0.0001147*I*I));
+}
+
+Double_t H1Boiling(Double_t I){
+  return (1 + (-0.008529*I) + (0.0001527*I*I));
+}
+
+Double_t He3Density(Double_t I){
+  return 0.0534*He3Boiling(I)/25;
+}
+
+Double_t H3Density(Double_t I){
+  return 0.077*H3Boiling(I)/25;
+}
+
+Double_t D2Density(Double_t I){
+  return 0.1422*D2Boiling(I)/25;
+}
+
+Double_t H1Density(Double_t I){
+  return 0.0708*H1Boiling(I)/25;
+}
 
 void get_density(int run, TString targ){
-  double density = getsqlcurrent(run);
+  double current = GetAvgCurrentSQL(run,0);
+  double density = 0;
+
+  if(targ=="H1"){
+    density=H1Density(current);
+  }else if(targ=="He3"){
+    density=He3Density(current);
+  }else if(targ=="H3"){
+    density=H3Density(current);
+  }else if(targ=="D2"){
+    density=D2Density(current);
+  }
 
   ofstream f(Form("batch_files/batch_%d",run));
   f << "export DENSITY=" << density << endl;
